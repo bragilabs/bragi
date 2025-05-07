f2b5feed04dc402dcc4cb3fcf19b99f455413f9f
Aron Kylebäck <aron@kyleback.com>
2025-05-07 11:40:46 +0200

    Removed Diesel

--- migrations/2024-12-05-203640_create_artist/up.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE artists (
-    artist_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
--- migrations/2024-12-05-203705_create_album/up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE albums (
-    album_id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    artist_id INTEGER NOT NULL REFERENCES artists(artist_id)
-)
